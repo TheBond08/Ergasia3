e4d007afd8f2116698f50e2cfc34465704ca538a
TheBond08 <bontislefteris@gmail.com>
2023-12-15 16:26:26 +0200

    comments

--- create_pipe.c
+++ create_pipe.c
@@ -2,24 +2,24 @@
 
 void create_pipe(int file){
     int pipes[2];
-    char buf[SIZE]; // Buffer is used to store the date received from the file
+    char buf[SIZE]; //Buffer is used to store the date received from the file
 
-    if(pipe(pipes) == -1){ // Checks if the pipe creation has succeeded
+    if(pipe(pipes) == -1){ //Checks if the pipe creation has succeeded
         printf("Pipe creation failed");
         exit(2);
     }
 
     pid_t pid = fork();
 
-    if(pid < 0){ // Checks if the fork process succeeded
+    if(pid < 0){ //Checks if the fork process has succeeded
         printf("Fork failed");
         exit(3);
     }
 
-    if (pid > 0){ // Parent process
+    if (pid > 0){ //Parent process
         close(pipes[1]);
 
-        if (dup2(pipes[0], STDIN_FILENO) == -1){
+        if (dup2(pipes[0], STDIN_FILENO) == -1){ //Redirecting stdin to read from the pipe
             printf("dup2 failed");
             exit(4);
         }
@@ -27,26 +27,26 @@ void create_pipe(int file){
         close(pipes[0]);
 
         while ((fgets(buf, SIZE, stdin)) != NULL){
-            printf("Data received through pipe %s", buf); // Prints the data received from the child process with the message
+            printf("Data received through pipe %s", buf); //Prints the data received from the child process with the message
         }
  
-        wait(NULL); // Waits for the child process to terminate
-    } else{ // Child process
+        wait(NULL); //Waits for the child process to terminate
+    } else{ //Child process
         close(pipes[0]);
 
-        if(dup2(pipes[1], STDOUT_FILENO) == -1){
+        if(dup2(pipes[1], STDOUT_FILENO) == -1){ //Redirecting the stdout to write of the pipe
             printf("dup2 failed");
             exit(4);
         }
 
         close(pipes[1]);
 
-        if(dup2(file, STDIN_FILENO) == -1){
+        if(dup2(file, STDIN_FILENO) == -1){ //Redirects the stdin to the file input
             printf("dup2 failed");
             exit(4);
         }
 
-        execl("/usr/bin/sort", "sort", "-n", (char *)NULL); // Runs the command sort -n for the file
+        execl("/usr/bin/sort", "sort", "-n", (char *)NULL); //Runs the command sort -n for the file
 
     }
 }
--- mypipe.c
+++ mypipe.c
@@ -9,12 +9,12 @@
 int main(int argc, char* argv[]){
     int file;
 
-    if((file = open(argv[1], O_RDONLY)) < 0){ // Opens the file to read and checks for errors
+    if((file = open(argv[1], O_RDONLY)) < 0){ //Opens the file to read and checks for errors
         printf("Error opening file\n");
         exit (1);
     }
     printf("Creating pipe\n");
-    create_pipe(file); // Calls the function to create the transfer pipe
+    create_pipe(file); //Calls the function to create the transfer pipe
     printf("Completed!\n");
 
     close(file);
